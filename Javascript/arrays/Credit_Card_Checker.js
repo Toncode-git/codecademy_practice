02389f62e05a0cfe84eac031b84d192c2b66b338
Toncode-git <alejandroantonioramirez@gmail.com>
2026-02-03 21:07:31 -0500

    Refactor validateCredit function to reverse input array and log results

--- Javascript/arrays/Credit_Card_Checker.js
+++ Javascript/arrays/Credit_Card_Checker.js
@@ -23,11 +23,12 @@ const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
 const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5];
 
 // Add your functions below:
-
 const validateCredit = arr => {
-    let reversed = mystery1.reverse().join(' ');
+    let reversed = arr.slice().reverse();
     return reversed;
 };
+console.log('\n');
+console.log(validateCredit(mystery1));
+console.log('\n');
 
-console.log(validateCredit(' \n' + mystery1));
 
